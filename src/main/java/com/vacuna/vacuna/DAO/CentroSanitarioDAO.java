1edf195048d1338f1046dc8aa9c78e04616ef57c
JAIME LEON ROSADO <Jaime.Leon2@alu.uclm.es>
2021-11-30 10:52:51 +0000

    Deleted src

--- src/main/java/com/vacuna/vacuna/DAO/CentroSanitarioDAO.java
+++
@@ -1,8 +0,0 @@
-package com.vacuna.vacuna.DAO;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.vacuna.vacuna.model.CentroSanitario;
-
-public interface CentroSanitarioDAO extends MongoRepository<CentroSanitario, String>{
-
-}
--- src/main/java/com/vacuna/vacuna/DAO/citaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.vacuna.vacuna.DAO;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.vacuna.vacuna.model.Cita;
-
-public interface citaDAO extends MongoRepository<Cita, String>{
-	
-}
