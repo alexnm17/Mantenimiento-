8844532641938045bd3837ed8ef4963ca2b7ee2e
Jaime León <jaimeleonro@gmail.com>
2021-12-05 13:24:42 +0100

    Añadida funcionalidad para definir el formato de vacunación

--- src/main/java/com/vacuna/vacuna/controller/FormatoVacunacionController.java
+++ src/main/java/com/vacuna/vacuna/controller/FormatoVacunacionController.java
@@ -1,12 +1,16 @@
 package com.vacuna.vacuna.controller;
 
 import java.util.Map;
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,6 +20,7 @@ import com.vacuna.vacuna.model.FormatoVacunacion;
 
 
 @RestController
+@RequestMapping("formato")
 public class FormatoVacunacionController {
 
 	@Autowired
@@ -46,4 +51,15 @@ public class FormatoVacunacionController {
 
 	}
 
+	@GetMapping("/getFormatoVacunacion")
+	public FormatoVacunacion getFormatoVacunacion() {
+		Optional<FormatoVacunacion> optFormato = formatoVacunacionDao.findById("Formato_Unico");
+		FormatoVacunacion formatoVacunacion = null;
+		if (optFormato.isPresent())
+			formatoVacunacion = optFormato.get();
+		else {
+			throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No existe un formato de Vacunacion definido");
+		}
+		return formatoVacunacion;
+	}
 }
