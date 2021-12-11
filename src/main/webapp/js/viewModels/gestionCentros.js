56a94bbda95b9dff861c21db4d544fad16dcf5ee
Jaime León <jaimeleonro@gmail.com>
2021-12-11 18:47:38 +0100

    Ahora se puede ver la lista de centros de salud

--- src/main/webapp/js/viewModels/gestionCentros.js
+++ src/main/webapp/js/viewModels/gestionCentros.js
@@ -1,23 +1,14 @@
 define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
-	'jquery', function(ko, app, moduleUtils, accUtils, $) {
+	'jquery'], function(ko, app, moduleUtils, accUtils, $) {
 
 
-		class GestionCenterViewModel {
+		class GestionUserViewModel {
 			constructor() {
 				var self = this;
 
 				self.centros = ko.observableArray([]);
-				self.id = ko.observable("");
-				self.nombre = ko.observable("");
-				self.horaInicio = ko.observable("");
-				self.horaFin = ko.observable("");
-				self.dosisTotales = ko.observable("");
-				self.aforo = ko.observable("");
-				self.localidad = ko.observable("");
-				self.provincia = ko.observable("");
-
-				self.nombreUsuario = ko.observable("");
 				self.tipoUsuario = ko.observable("");
+				self.nombreUsuario = ko.observable("");
 
 				// Header Config
 				self.headerConfig = ko.observable({
@@ -25,35 +16,23 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 					'viewModel': null
 				});
 				moduleUtils.createView({
-					'viewPath': 'views / header.html'
+					'viewPath': 'views/header.html'
 				}).then(function(view) {
-						self.headerConfig({
-							'view': view,
+					self.headerConfig({
+						'view': view,
 						'viewModel': app.getHeaderModel()
-						})
 					})
-			}
-
-			comprobarRol() {
-				let self = this;
-				let data = {
-					url: "login/comprobarRolAdmin",
-					type: "get",
-					contentType: 'application/json',
-					success: function(response) {
-						if (response == "denegado") {
-							app.router.go({ path: "login" });
-						}
-					},
-					error: function(response) {
-						$.confirm({ title: 'Error', content: response.responseJSON.message,  type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar',  btnClass: 'btn-red', action: function() { }    }    );
-
-					}
-				};
-				$.ajax(data);
+				})
 			}
 
 
+			enable() {
+				document.getElementById("menuCentros").disabled = false;
+			}
+
+			disable() {
+				document.getElementById("menuCentros").disabled = true;
+			}
 
 			gestionUsuarios() {
 				app.router.go({ path: "gestionUsuarios" });
@@ -92,168 +71,73 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 				let data = {
 					url: "centro/getTodos",
 					type: "get",
-					contentTyp: 'application/json',
-					succes: function(response) {
-						self.centros([]);
-						for (let i = 0;     response.length; i++) {
-	let centro = {
-		i: response[i].id,
-		nombre: response[i].nombre,
-		dosisTotales: response[i].dosisTotales,
-		afor: response[i].aforo,
-		horaInici: response[i].horaInicio,
-		horaFi: response[i].horaFin,
-		localidad: response[i].localidad,
-		provinci: response[i].provincia,
-		elimina: function() {
-			self.eliminarUsuario(response[i].dni)
-		},
-
-		modificarCentro: function() {
-			app.centro = this;
-			app.router.go({ path: "modificarCentro" });
-		},
-
-	};
-
-	self.centros.push(centro);
-}
-if (response.length 0 )  {
-	document.getElementById("horaInicio").style.display = 'none';
-	document.getElementById("aforo").style.display = 'none';
-	document.getElementById("horaFin").style.display = 'none';
-  else {
-	document.getElementById("horaInicio").style.display = 'inline';
-	document.getElementById("aforo").style.display = 'inline';
-	document.getElementById("horaFin").style.display = 'inline';
-}
+					contentType: 'application/json',
+					success: function(response) {
+						self.centros(response);
 					},
-error: function(response) {
-	$.confirm({ title: 'Error',  content: response.responseJSON.message,  type: 'red',  typeAnimated: rue,   buttons:    tryAgain:{   text: 'Cerrar', btnClass: 'btn-red', action: function() { }  }  } });
-
-}
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+					}
 				};
-$.ajax(data);
+				$.ajax(data);
 			}
 
-logout() {
-	let self = this;
-	let data = {
-		url: "login/logout",
-		type: "post",
-		contentType: 'application/json',
-		success: function(response) {
-			app.router.go({ path: "login" });
-		},
-		error: function(response) {
-			$.confirm({ title: 'Error'  content: response.responseJSON.message,  type: 'red',  typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red',  action: function()  { } } } });
+			logout() {
+				let self = this;
+				let data = {
+					url: "login/logout",
+					type: "post",
+					contentType: 'application/json',
+					success: function(response) {
+						app.router.go({ path: "login" });
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
 
-		}
-	};
-	$.ajax(data);
-}
-
-eliminarCentro(id) {
-	let self = this;
-	let data = {
-		url: "centro/eliminarCentro/" + id,
-		typ: "delete",
-		contentType: 'application/json',
-
-		succes: function(response) {
-			console.log(response);
-
-			$.confirm({
-				title: 'Confirmado',
-				content: 'Centro eliminado',
-				type: 'green',
-				typeAnimated: true,
-				buttons: {
-					Cerrar: function() {
 					}
-				}
-			});
-			self.getCentros();
-			console.log(self.error);
-			self.getCentros();
-		},
-		error: function(response) {
-			$.confirm({
-				title: 'Error',
-				content: response.responseJSON.message,
-				type: 'red',
-				typeAnimated: true,
-				buttons: {
-					tryAgain: {
-						text: 'Cerrar',
-						btnClass: 'btn-red',
-						action: function() {
-						}
-					}
-				}
-			});
-
-			console.log(response.responseJSON.message);
-
-		}
-
-	};
-	$.confirm({
-		title: 'Eliminar',
-		content: '¿Seguro que desea eliminar?',
-		buttons: {
-			Confirmar: functio() {
-		$.ajax(data);
-	},
-		Cancelar: functio() {
-	},
-					}
-		});
-
+				};
+				$.ajax(data);
 			}
 
+			modificarCentro(id) {
+				app.idc = id;
+				app.centro = this;
+				app.router.go({ path: "modificarCentro" });
+			}
 
-modificarCentro(id) {
-	app.idc = id;
-	app.centro = this;
-	app.router.go({ path: "modificarCentro" });
-}
+			getUserConnect() {
+				let self = this;
+				let data = {
+					url: "login/getUser",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						self.nombreUsuario(response[0]);
+						self.tipoUsuario(response[1]);
+					},
+					error: function(response) {
+						self.error(response.responseJSON.errorMessage);
 
-getUserConnect() {
-	let self = this;
-	let data = {
-		url: "login/getUser",
-		type: "get",
-		contentType: 'application/json',
-		success: function(response) {
+					}
+				};
+				$.ajax(data);
+			}
 
-			self.nombreUsuario(response[0]);
-			self.tipoUsuario(response[1]);
-		},
-		error: function(response) {
-			$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true,  buttons: {  tryAgain: {   text: 'Cerrar',  btnClass: 'btn-red', action: function(    }      );
+			connected() {
+				accUtils.announce('Inicio page loaded.');
+				document.title = "Gestion Usuarios";
+				this.getCentros();
+				this.getUserConnect();
+			};
 
-		}
-	};
-	$.ajax(data);
-}
+			disconnected() {
+				// Implement if needed
+			};
 
-connected() {
-	accUtils.announce('Inicio page loaded.');
-	document.title = "Gestion Centros";
-	this.comprobarRol();
-	this.getCentros();
-	this.getUserConnect();
-};
-
-disconnected() {
-	// Implement if needed
-};
-
-transitionCompleted() {
-	// Implement if needed
-};
+			transitionCompleted() {
+				// Implement if needed
+			};
 		}
 
-return GestionCenterViewModel;
+		return GestionUserViewModel;
 	});
