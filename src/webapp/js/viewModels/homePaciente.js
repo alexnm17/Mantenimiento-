067ab0ecd1f9f29ee76d11310a37f1291942c4f6
Jaime León <jaimeleonro@gmail.com>
2021-12-22 14:12:14 +0100

    cambio en los directorios

---
+++ src/webapp/js/viewModels/homePaciente.js
@@ -0,0 +1,274 @@
+define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
+	'jquery'], function(ko, app, moduleUtils, accUtils, $) {
+
+
+		class HomePacienteViewModel {
+			constructor() {
+				var self = this;
+				self.user = app.user;
+				self.botonCita = ko.observable(1);
+				self.botonFecha = ko.observable(2);
+				self.tablaCita = ko.observable(1);
+				self.usuarios = ko.observableArray([]);
+				self.nombre = ko.observable("");
+				self.apellidos = ko.observable("");
+				self.dniUsuario = ko.observable("");
+				self.emailUsuario = ko.observable("");
+				self.tipoUsuario = ko.observable("");
+				self.centroAsignado = ko.observable("");
+				self.dosisAdministradas = ko.observable("");
+
+				self.cita = ko.observable("");
+				self.fechaPrimeraDosis = ko.observable("");
+				self.fechaSegundaDosis = ko.observable("");
+				self.citas = ko.observableArray([]);
+
+				self.mensaje = ko.observable(2);
+				self.mostrarSolicitarCita = ko.observable(1);
+				self.getCentros();
+
+
+				var hoy = new Date();
+				self.fecha = ko.observable(hoy.toLocaleString().split(' ')[0]);
+
+
+				// Header Config
+				self.headerConfig = ko.observable({
+					'view': [],
+					'viewModel': null
+				});
+				moduleUtils.createView({
+					'viewPath': 'views/header.html'
+				}).then(function(view) {
+					self.headerConfig({
+						'view': view,
+						'viewModel': app.getHeaderModel()
+					})
+				})
+			}
+
+			getCentros() {
+				let self = this;
+				let data = {
+					url: "centro/getTodos",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						for (let c of response) {
+							let centro = {
+								id: c.id,
+								nombre: c.nombre,
+								dosisTotales: c.dosisTotales,
+								localidad: c.localidad,
+								provincia: c.provincia,
+								eliminar: function() {
+									self.eliminarUsuario(centro.dni);
+								},
+
+								modificarCentros: function() {
+									app.centro = this;
+									app.router.go({ path: "modificarCentro" });
+								},
+							};
+						}
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+			solicitarCita() {
+				alert("La petición de cita se está procesando. Espere para la resolución.")
+				let self = this;
+				let info = {
+					email: self.emailUsuario(),
+				};
+
+				let data = {
+					data: JSON.stringify(info),
+					url: "cita/solicitarCita",
+					type: "post",
+					contentType: 'application/json',
+					success: function(response) {
+						$.confirm({
+							title: 'Confirmado',
+							content: 'Cita Creada',
+							type: 'green',
+							typeAnimated: true,
+							buttons: {
+								Cerrar: function() {
+									location.reload();
+								}
+							}
+						});
+						self.tablaCita(1);
+						self.getCitaPaciente();
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+			logout() {
+				localStorage.clear();
+				app.router.go({ path: "login" });
+			}
+
+
+			getCitaPaciente() {
+				let self = this;
+				let data = {
+					url: "cita/getCitaPaciente/" + self.dniUsuario(),
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						self.citas(response);
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+
+			eliminarCita(id) {
+				let self = this;
+				let data = {
+					url: "cita/anularCita/" + id,
+					type: "delete",
+					contentType: 'application/json',
+					success: function(response) {
+
+						$.confirm({
+							title: 'Confirmado',
+							content: 'Cita eliminada',
+							type: 'green',
+							typeAnimated: true,
+							buttons: {
+								Cerrar: function() {
+								}
+							}
+						});
+						self.getCitaPaciente();
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.confirm({
+					title: 'Eliminar',
+					content: '¿Seguro que desea eliminar?',
+					buttons: {
+						Confirmar: function() {
+							$.ajax(data);
+						},
+						Cancelar: function() {
+						},
+					}
+				});
+			}
+			comprobarRol() {
+				let self = this;
+				let data = {
+					url: "login/comprobarRolPaciente",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						if (response == "denegado") {
+							app.router.go({ path: "login" });
+						}
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+			getUserConnect(onComplete) {
+
+
+
+				let self = this;
+				let data = {
+					url: "login/getUser",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						self.nombre(response[0]);
+						self.tipoUsuario(response[1]);
+						self.emailUsuario(response[2]);
+						self.centroAsignado(response[3]);
+						self.dniUsuario(response[4]);
+						self.dosisAdministradas(response[5]);
+						onComplete();
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+			primerInicio() {
+				let self = this;
+				let data = {
+					url: "login/primerInicio",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						console.log(response);
+						if (response == "1") {
+							app.router.go({ path: "changePassword" });
+						}
+					},
+					error: function(response) {
+					}
+				};
+				$.ajax(data);
+			}
+
+
+			modificarCita(idCita, dniPaciente) {
+				localStorage.setItem("idCitaAModificar", idCita)
+				localStorage.setItem("dniUsuarioAAdministrar", dniPaciente)
+				localStorage.setItem("emailUsuarioAAdministrar", localStorage.getItem("emailUsuario"))
+
+				app.router.go({ path: "modificarCita" });
+			}
+
+
+			connected() {
+				accUtils.announce('Inicio page loaded.');
+				document.title = "Inicio";
+				this.getUserConnect(() => {
+					this.comprobarRol();
+					this.primerInicio();
+					this.getCitaPaciente();
+				});
+
+
+			};
+
+			disconnected() {
+				// Implement if needed
+			};
+
+			transitionCompleted() {
+				// Implement if needed
+			};
+		}
+
+		return HomePacienteViewModel;
+	});
---
+++ src/webapp/js/viewModels/homePersonalDeCitas.js
@@ -0,0 +1,156 @@
+define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
+	'jquery'], function(ko, app, moduleUtils, accUtils, $) {
+
+
+		class GestionUserViewModel {
+			constructor() {
+				var self = this;
+
+				self.usuarios = ko.observableArray([]);
+				self.nombre = ko.observable("");
+				self.email = ko.observable("");
+				self.dni = ko.observable("");
+				self.tipoUsuario = ko.observable("");
+				self.password = ko.observable("");
+				self.centroAsignado = ko.observable("");
+				self.localidad = ko.observable("");
+				self.provincia = ko.observable("");
+
+
+				self.nombreUsuario = ko.observable("");
+
+				// Header Config
+				self.headerConfig = ko.observable({
+					'view': [],
+					'viewModel': null
+				});
+				moduleUtils.createView({
+					'viewPath': 'views/header.html'
+				}).then(function(view) {
+					self.headerConfig({
+						'view': view,
+						'viewModel': app.getHeaderModel()
+					})
+				})
+			}
+
+
+			enable() {
+				document.getElementById("menuCentros").disabled = false;
+			}
+
+			disable() {
+				document.getElementById("menuCentros").disabled = true;
+			}
+
+			irHomePersonalDeCitas() {
+				app.router.go({ path: "homePersonalDeCitas" });
+			}
+
+			administrarCitas(dni, email) {
+				localStorage.setItem('dniUsuarioAAdministrar', dni)
+				localStorage.setItem('emailUsuarioAAdministrar', email)
+				app.router.go({ path: "administrarCitas" });
+			}
+
+			paginaInicio() {
+				app.router.go({ path: "homeAdmin" });
+			}
+
+
+			getUsuarios() {
+				let self = this;
+				let data = {
+					url: "Usuario/getTodos",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						self.usuarios([]);
+						for (let usuario of response) {
+							let paciente = {
+								nombre: usuario.nombre,
+								email: usuario.email,
+								dni: usuario.dni,
+								tipoUsuario: usuario.tipoUsuario,
+								centroAsignado: usuario.centroAsignado,
+								eliminar: function() {
+									self.eliminarUsuario(usuario.email);
+								},
+								modificarUsuarios: function() {
+									app.paciente = this;
+									app.router.go({ path: "modificarUsuario" });
+								},
+							};
+							self.usuarios.push(paciente);
+						}
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+			logout() {
+				localStorage.clear();
+				app.router.go({ path: "login" });
+			}
+
+			getUserConnect() {
+				let self = this;
+				let data = {
+					url: "login/getUser",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+
+						self.nombreUsuario(response[0]);
+						self.tipoUsuario(response[1]);
+					},
+					error: function(response) {
+						self.error(response.responseJSON.errorMessage);
+
+					}
+				};
+				$.ajax(data);
+			}
+			
+			comprobarRol() {
+				let self = this;
+				let data = {
+					url: "login/comprobarRolPersonalDeCitas",
+					type: "get",
+					contentType: 'application/json',
+					success: function(response) {
+						if (response == "denegado") {
+							app.router.go({ path: "login" });
+						}
+					},
+					error: function(response) {
+						$.confirm({ title: 'Error', content: response.responseJSON.message, type: 'red', typeAnimated: true, buttons: { tryAgain: { text: 'Cerrar', btnClass: 'btn-red', action: function() { } } } });
+
+					}
+				};
+				$.ajax(data);
+			}
+
+			connected() {
+				accUtils.announce('Inicio page loaded.');
+				document.title = "Gestion Usuarios";
+				this.comprobarRol();
+				this.getUsuarios();
+				this.getUserConnect();
+			}
+
+			disconnected() {
+				// Implement if needed
+			}
+
+			transitionCompleted() {
+				// Implement if needed
+			}
+		}
+
+		return GestionUserViewModel;
+	});
